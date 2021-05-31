da9b3f7ef4ee5e8c2644521521209a9065ff8a04
Mahas1 <82939599+Mahas1@users.noreply.github.com>
2021-05-31 11:41:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,152 @@
+import random
+
+import discord
+from discord.ext import commands
+
+import details
+from Commands import quotes
+from Commands import links
+from Commands import time
+from Commands import wish_good
+from Commands.ask import ask_qn
+from Commands import stupid_bot
+from Commands import random_events
+from Commands import encourage
+
+prefix = details.server_prefix
+token = 'ODQ4NTI5NDIwNzE2ODY3NjI1.YLN8mg.1F6QbPjOJR4w1xF-LsJel2WU06Q'
+rickroll = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix=prefix, intents=intents, help_command=None)
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is online!')
+    await client.change_presence(activity=discord.Streaming(name=f'{details.server_prefix}help', url=rickroll))
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.reply('Hello!')
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.reply('Pong!')
+
+
+@client.command()
+async def kill(ctx):
+    await ctx.send('closing client')
+    await client.close()
+
+
+@client.command()
+async def userid(ctx, target: discord.Member = None):
+    if target:
+        await ctx.reply(target.id)
+    else:
+        await ctx.reply(ctx.author.id)
+
+
+@client.command()
+async def inspire(ctx):
+    await ctx.send(quotes.get_quote())
+
+
+@client.command()
+async def link(ctx, link):
+    if link.lower() == 'list':
+        await ctx.send(links.link_list())
+    else:
+        await ctx.send(links.link_message(link))
+
+
+@client.command()
+async def goodmorning(ctx):
+    await ctx.send(wish_good.good_morning(ctx.message.content, ctx))
+
+
+@client.command()
+async def goodafternoon(ctx):
+    await ctx.send(wish_good.good_afternoon(ctx.message.content, ctx))
+
+
+@client.command()
+async def goodevening(ctx):
+    await ctx.send(wish_good.good_evening(ctx.message.content, ctx))
+
+
+@client.command()
+async def goodnight(ctx):
+    await ctx.send(wish_good.good_night(ctx.message.content, ctx))
+
+
+@client.command(aliases=['flip'])
+async def flip_command(ctx):
+    await ctx.reply(random_events.coin_flip())
+
+
+@client.command()
+async def ask(ctx):
+    await ctx.reply(ask_qn(ctx.message.content))
+
+
+@client.command()
+async def roll(ctx):
+    await ctx.reply(random_events.roll_dice())
+
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(colour=discord.Colour.blue())
+    embed.set_author(name='List of commands')
+    embed.add_field(name=f'{prefix}ping', value='Replies with "Pong!"', inline=False)
+    embed.add_field(name=f'{prefix}hello', value='Says hello', inline=False)
+    embed.add_field(name=f'{prefix}userid [user(optional)]', value='Replies with the user\'s ID', inline=False)
+    embed.add_field(name=f'{prefix}inspire', value='Sends a random quote', inline=False)
+    embed.add_field(name=f'{prefix}link [name]', value=f'Use `{prefix}link list` for a list of links', inline=False)
+    embed.add_field(name=f'{prefix}goodmorning', value='Wishes a good morning to you, because it\'s your friend', inline=False)
+    embed.add_field(name=f'{prefix}goodafternoon', value='Wishes a good afternoon to you, because it\'s your friend', inline=False)
+    embed.add_field(name=f'{prefix}goodevening', value='Wishes a good evening to you, because it\'s your friend', inline=False)
+    embed.add_field(name=f'{prefix}goodnight', value='Wishes a good night to you, because it\'s your friend', inline=False)
+    embed.add_field(name=f'{prefix}flip', value='Flips a coin, and tells you the result', inline=False)
+    embed.add_field(name=f'{prefix}roll', value='Rolls a die, and tells you the result', inline=False)
+    embed.add_field(name=f'{prefix}ask', value='Gives honest opinion in the form of yes/no', inline=False)
+    await ctx.send(embed=embed)
+
+
+@client.event
+async def on_message(message):
+    msg = message.content.lower()
+
+    if message.author == client.user:
+        return
+    if len(msg.split()) == 1:
+        if msg in ['sparkle', 'sparkles']:
+            await message.reply('✨')
+    else:
+        if any(word in msg for word in [' sparkle', 'sparkle ', ' sparkles', 'sparkles ']):
+            await message.reply('✨')
+    await client.process_commands(message)
+
+    if msg.startswith(f'{prefix}time'):
+        await message.reply(time.time_bm(msg))
+
+    if any(word in msg for word in stupid_bot.stupid_bot) and any(word in msg for word in stupid_bot.bot_call):
+        if any(word not in msg for word in stupid_bot.stupid_exception):
+            await message.reply(random.choice(stupid_bot.stupid_bot_revenge))
+
+    if any(word in msg for word in ['845225811152732179']):  # respond to pings
+        if not any(word in msg for word in stupid_bot.stupid_bot):
+            await message.reply('I heard my name. what\'s up ?')
+
+    if any(word in msg for word in encourage.sad_words):  # encourages you if you type a sad message
+        if any(word in msg for word in ['I', 'i', 'im']):
+            if not any(word in msg for word in ['not']):
+                await message.reply(random.choice(encourage.encouragement))
+
+
+if __name__ == '__main__':
+    client.run(token)
